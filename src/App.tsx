ac1034591681754e8af78f5cfaf3d7c3086c500b
ermannos <e.scanagatta@gmail.com>
2020-11-28 11:08:26 +0100

    Revert "Updates"
    This reverts commit 78eb418c7a2b5086eccce906aadf8c31f2078e75.

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Page from './pages/Page';
+import Header from './pages/Header';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <Router basename="/covid-19-italy">
+      <Header title="Covid-19 Italia" />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/page">
+          <Page />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
